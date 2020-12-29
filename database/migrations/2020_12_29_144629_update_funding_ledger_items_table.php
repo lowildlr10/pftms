99b1af3a10d66defe2a7271926efd98d0134d320
lowildlr10 <lowildlr10@gmail.com>
2020-12-29 14:52:44 +0800

    Added the mooe_classifications and mooe_account_titles table; Updated the funding_ledger_items

---
+++ database/migrations/2020_12_29_144629_update_funding_ledger_items_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class UpdateFundingLedgerItemsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('funding_ledger_items', function (Blueprint $table) {
+            $table->unsignedInteger('order_no')->after('ors_id');
+            $table->uuid('mooe_account_title_id')->after('ors_id');
+            $table->foreign('mooe_account_title_id')->references('id')->on('mooe_account_titles');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('funding_ledger_items', function (Blueprint $table) {
+            $table->dropColumn('order_no');
+            $table->dropColumn('mooe_account_title_id');
+        });
+    }
+}
